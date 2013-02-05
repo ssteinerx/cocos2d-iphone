670a4524ec8045d822293344095f087350f505de
Steve Steiner <ssteinerX@gmail.com>
2013-02-05 18:54:45 -0500

    Added new "nextBackRestartAction.h" with 30-ish lines of code duplicated across many test files.

---
+++ nextBackRestartAction.h
@@ -0,0 +1,32 @@
+Class nextAction(void);
+Class backAction(void);
+Class restartAction(void);
+
+Class nextAction()
+{
+    
+	sceneIdx++;
+	sceneIdx = sceneIdx % ( sizeof(transitions) / sizeof(transitions[0]) );
+	NSString *r = transitions[sceneIdx];
+	Class c = NSClassFromString(r);
+	return c;
+}
+
+Class backAction()
+{
+	sceneIdx--;
+	int total = ( sizeof(transitions) / sizeof(transitions[0]) );
+	if( sceneIdx < 0 )
+		sceneIdx += total;
+    
+	NSString *r = transitions[sceneIdx];
+	Class c = NSClassFromString(r);
+	return c;
+}
+
+Class restartAction()
+{
+	NSString *r = transitions[sceneIdx];
+	Class c = NSClassFromString(r);
+	return c;
+}
